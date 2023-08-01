1139879889a541f8d6c84a79369ef77fc160aed0
rundimeco <rundimeco@gmail.com>
2023-08-01 12:31:39 +0200

    wrapping in functions

--- test_sim.py
+++ test_sim.py
@@ -12,9 +12,16 @@ def get_model_path():
   os.system(f"wget {url} -O {path}")
   return path
 
-def get_sentence_thematic(vocab, sentence):
+def classify_sentences(sentence_list):
   model_path = get_model_path()
   model = KeyedVectors.load_word2vec_format(model_path, binary=True, unicode_errors="ignore")
+  vocab = get_extended_voc(language = "fr")
+  list_thematics = []
+  for sentence in sentence_list:
+    list_thematics.append(get_sentence_thematic(vocab, model, sentence))
+  return list_thematics
+
+def get_sentence_thematic(vocab, model, sentence):
   liste_res = []
   for thematic, word_list in vocab.items():
       #thematic_sentence = " ".join(word_list)
@@ -55,26 +62,28 @@ def extend_vocabulary(liste, min_sim = 0.5, min_nb = 20):
   #print(json.dumps(dic, indent = 2, ensure_ascii=False))
   return dic
 
-def create_extended_voc(language = "fr", force = False):
+def get_extended_voc(language = "fr", force = False):
   #TODO use langauge parameter
   voc_dir = "vocabularies"
   base_name = f"test_voc_{language}.json"
   base_voc_path = f"{voc_dir}/{base_name}"
-  extended_voc_path = f"{voc_dir}/extended_{base_name}", "w"
+  extended_voc_path = f"{voc_dir}/extended_{base_name}"
   if os.path.exists(extended_voc_path):
+    print("Loading vocabulary...\n")
     with open(extended_voc_path) as f:
         extended_voc = json.load(f)
   else:
+    print("Creating vocabulary...\n")
     with open(base_voc_path) as f:
       liste = json.load(f)
     extended_voc = extend_vocabulary(liste)
-    with open(extended_voc_path) as w:
+    with open(extended_voc_path, "w") as w:
       w.write(json.dumps(extended_voc, indent =2))
   return extended_voc
 if __name__=="__main__":
   import json
-  extended_voc = create_extended_voc(language = "fr"):
-
-  for sentence in ["J'aime le sport", "En sport moi c'est le foot", "Je kiffe Jésus et les églises", "toto titi"]:
-    print(sentence)
-    print(get_sentence_thematic(extended_voc, sentence))
+  sentences = ["J'aime le sport", "En sport moi c'est le foot", "Je kiffe Jésus et les églises", "toto titi"]
+  thematics = classify_sentences(sentences)
+  for sent, theme in zip(sentences, thematics):
+    print(sent)
+    print("-->", theme)
